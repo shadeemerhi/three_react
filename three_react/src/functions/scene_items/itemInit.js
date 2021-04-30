65de797615801beedf04f2175c683fe4e4bbeb5e
Shadee Merhi <shadmerhi@gmail.com>
2021-04-29 21:18:37 -0700

    create object add repitition function

--- three_react/src/functions/scene_items/itemInit.js
+++ three_react/src/functions/scene_items/itemInit.js
@@ -6,5 +6,30 @@ export const addItems = (self) => {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
+    const cube2 = new THREE.Mesh(geometry, material);
+    // self.current.scene.add(cube); // ONLY ADDS AN OBJECT ONCEc
+    // self.current.scene.add(cube);
+    // self.current.scene.add(cube);
+    cube.name = 'lolCube'
     self.current.scene.add(cube);
+
+    addCubes(self, [{}, {}, {}]);
+
+    // const cubeFromScene = self.current.scene.getObjectByName('lcdcdolCube');
+    // console.log('we found', cubeFromScene);
+
+
+}
+
+
+// Showing that duplicate objects can be added if created as different variables
+const addCubes = (self, objects) => {
+
+    for(const object of objects) {
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        const cube = new THREE.Mesh(geometry, material);
+        self.current.scene.add(cube);
+
+    }
 }
